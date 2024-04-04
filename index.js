b399fe1e87c0b5015f38ef5a0690ae32548de021
JAMES WAINAINA MBUGUA <105612401+jammie-commits@users.noreply.github.com>
2024-04-04 18:09:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function() {
+    alert('I was clicked!'); 
+    
+
+});
+input.addEventListener('click', clickAlert);
 function addingEventListener() {
+    const myButton = document.getElementById("button");
+
+    myButton.addEventListener("click", function() {
+      
+    });
 }
